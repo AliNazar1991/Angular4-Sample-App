bc7ef73cbe25bd2d32e3ef67bab0c0d53e2a4c37
Ali Nazar <ali_nazar91@hotmail.se>
2018-07-12 21:02:54 +0200

    working solution now with bootstrap 3.3.7

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { VolumesComponent } from './volumes/volumes.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    VolumesComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/volumes/volumes.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-volumes',
+  template: `<button class="btn btn-primary"> Load volumes </button>`
+})
+export class VolumesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
