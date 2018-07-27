6e92fa857dbdb7310be8d091b5d34e2043658c31
AliNazar <ali_nazar91@hotmail.se>
2018-07-27 08:07:36 +0200

    more things added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { HttpModule } from '@angular/http'
 import { AppComponent } from './app.component';
 import { VolumesComponent } from './volumes/volumes.component';
 import { FavoriteComponent } from './favorite/favorite.component';
@@ -12,6 +12,7 @@ import { ContactFormComponent } from './contact-form/contact-form.component';
 import { FormsModule, ReactiveFormsModule} from '@angular/forms';
 import { SignupFormComponent } from './signup-form/signup-form.component';
 import { ChangePasswordComponent } from './change-password/change-password.component';
+import { PostsComponent } from './posts/posts.component';
 
 @NgModule({
   declarations: [
@@ -24,13 +25,15 @@ import { ChangePasswordComponent } from './change-password/change-password.compo
     ZippyComponent,
     ContactFormComponent,
     SignupFormComponent,
-    ChangePasswordComponent, 
+    ChangePasswordComponent,
+    PostsComponent, 
 
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    ReactiveFormsModule 
+    ReactiveFormsModule,
+    HttpModule 
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/posts/posts.component.ts
@@ -0,0 +1,31 @@
+import { Component, OnInit } from '@angular/core';
+import { Http } from '../../../node_modules/@angular/http';
+import { Alert } from '../../../node_modules/@types/selenium-webdriver';
+
+@Component({
+  selector: 'posts',
+  templateUrl: './posts.component.html',
+  styleUrls: ['./posts.component.css']
+})
+export class PostsComponent{
+  posts: any[];
+  private url = 'https://jsonplaceholder.typicode.com/posts/';
+  
+  constructor(private http: Http) { 
+    http.get(this.url)
+      .subscribe(response =>{
+        this.posts = response.json();
+      });
+  }
+
+  createPost(input : HTMLInputElement){
+    let post = {title: input.value }
+    input.value='';
+    this.http.post(this.url,JSON.stringify(post))
+      .subscribe(response =>{
+        post['id'] = response.json().id;
+        this.posts.splice(0,0,post);
+      });
+  }
+
+}
